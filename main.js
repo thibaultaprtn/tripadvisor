68f80b0999f9bcb6b58c7c1a37b0c92acdad4e60
Thibault Apretna <thibault.apretna@gmail.com>
2024-08-18 16:57:47 +0200

    test de submit

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-const $ = document;
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("document loaded correctly");
 
-$.addEventListener("DOMContentLoaded", () => {
-  console.log("document loaded");
+  document
+    .querySelector("#contactForm")
+    .addEventListener("submit", async (e) => {
+      e.preventDefault();
+
+      console.log("Le bouton submit fonctionne");
+
+      const data = {
+        firstname: document.querySelector("#fname").value,
+        lastname: document.querySelector("#lname").value,
+        email: document.querySelector("#email").value,
+        subject: document.querySelector("#subject").value,
+        message: document.querySelector("#message").value,
+      };
+
+      console.log(data);
+    });
 });
